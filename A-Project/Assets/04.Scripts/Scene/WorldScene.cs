b4d22e9621b28782561cbde96d3b485def8ef0fe
이상신 <dygp25@naver.com>
2023-10-25 06:24:09 +0900

    feat: "BaseScene 추가"
    Related to: #5

---
+++ A-Project/Assets/04.Scripts/Scene/WorldScene.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldScene : BaseScene
+{
+    protected override void Init()
+    {
+
+    }
+
+    protected override void Clear()
+    {
+
+    }
+}
